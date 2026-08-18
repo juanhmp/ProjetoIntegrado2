3f093a89b964fc3584286592978f2fd8411a0c99
juanhmp <lualeomen@gmail.com>
2026-08-18 13:44:29 -0300

    Adiciona dependencias Python para Vercel

--- CS/obj/Debug/net10.0/PI.AssemblyInfo.cs
+++ CS/obj/Debug/net10.0/PI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf55c315cf455a87ad176827931477e5cab300d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251c86ad7344aa041aa0d13d24859de22e4bf1f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

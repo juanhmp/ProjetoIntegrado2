f1803e289df16650b84fe76eea71b142ebf9fa4b
juanhmp <lualeomen@gmail.com>
2026-08-18 08:51:59 -0300

    commit

--- CS/obj/Debug/net10.0/PI.AssemblyInfo.cs
+++ CS/obj/Debug/net10.0/PI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366378cf35a34a13f8c50cb1e4e272585f4fdd49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d460fe1beddd3ba9af4724478979a5a470fda8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

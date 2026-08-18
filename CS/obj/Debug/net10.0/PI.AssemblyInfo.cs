d606c81eb54afab19f0c9b27923ea282f5eb6933
juanhmp <lualeomen@gmail.com>
2026-08-18 14:07:11 -0300

    Organiza projeto local e Vercel

--- CS/obj/Debug/net10.0/PI.AssemblyInfo.cs
+++ CS/obj/Debug/net10.0/PI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f093a89b964fc3584286592978f2fd8411a0c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8916887d00428c9070a40161b418414d57405e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
